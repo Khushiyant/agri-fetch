9f2ac599837a076609475f7d1a8550a72d9a3ccc
Khushiyant <69671407+Khushiyant@users.noreply.github.com>
2022-07-02 01:29:18 +0530

    modified query data, added path to view query

--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
+import json
 from urllib import response
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import json
 
 @api_view(['GET'])
 def getData(request):
-    person = {"name": "Hello"}
-    return Response(person)
+    pass
+
+
+def getQueryData(request):
+    query = open("static/json/query.json", "r").read()
+    return Response(query)
