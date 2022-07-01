163c859e417d52286b3adfd09644fead28102334
Khushiyant <69671407+Khushiyant@users.noreply.github.com>
2022-07-02 01:18:35 +0530

    added scrapped query data, added api app config

---
+++ api/views.py
@@ -0,0 +1,8 @@
+from urllib import response
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getData(request):
+    person = {"name": "Hello"}
+    return Response(person)
