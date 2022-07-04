c9ab34cefa63eff5593779cb0d1e9b33af710bb9
Khushiyant <69671407+Khushiyant@users.noreply.github.com>
2022-07-04 19:20:31 +0530

    added database for query data, instead of json

--- base/models.py
+++ base/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Commodity(models.Model):
     name = models.CharField(max_length=60)
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
 
 class State(models.Model):
     name = models.CharField(max_length=60)
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
 
 class Market(models.Model):
     name = models.CharField(max_length=60)
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
